778af8c07fb15f4539e018f1774225335c237f49
haci karahasanoglu <haci@hacikho.com>
2025-05-08 21:31:54 -0400

    changes

--- stock_analizer/stage2.py
+++ stock_analizer/stage2.py
@@ -7,9 +7,50 @@ from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 from cachetools import TTLCache
 
-# Load environment variables
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
+
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    secret_name = "polygon_api"
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+    # Your code goes here.
+
+env = os.getenv("ENVIRONMENT", "local").lower()
+
+if env == "aws":
+    api_key = get_secret()
+else:
+    # Load .env variables for local development
+    load_dotenv()
+    API_KEY = os.getenv('API_KEY')
+
 
 app = FastAPI()
 
